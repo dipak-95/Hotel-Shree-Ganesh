1d89a939a70d2261693ead766163835f23c4c6ec
dipak-95 <dipakladani95@gmail.com>
2025-12-17 11:26:12 +0530

    Add auto-seed admin and rooms

--- server/models/Room.js
+++ server/models/Room.js
@@ -4,8 +4,8 @@ const RoomSchema = new mongoose.Schema({
     type: { type: String, required: true }, // e.g., '2-Bed', '3-Bed', '4-Bed'
     price: { type: Number, required: true },
     totalRooms: { type: Number, required: true },
-    // You could track available rooms per date, but for simplicity we'll dynamic check or just store base inventory
-    description: { type: String }
+    description: { type: String },
+    images: [String]
 });
 
 module.exports = mongoose.model('Room', RoomSchema);
